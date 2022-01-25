096d4475320c2d6b899f759f832afa837136f011
Alexandre JURQUET <a.jurquet@gmail.com>
2022-01-25 17:24:37 +0100

    projet terminé, flake8 ok

--- bruteforce.py
+++ bruteforce.py
@@ -1,48 +1,24 @@
 import operator
-import csv
-import profile
 import sys
-from memory_profiler import profile
 
 sys.setrecursionlimit(1500)
 
-# @profile
-def glouton(donnees):
-    # Classe les actions par la meilleure rentabilité et les ajoute jusqu'à la limite de 500
 
-    donnees = [
-        ["action_01", 20, 0.05],
-        ["action_02", 30, 0.1],
-        ["action_03", 50, 0.15],
-        ["action_04", 70, 0.2],
-        ["action_05", 60, 0.17],
-        ["action_06", 80, 0.25],
-        ["action_07", 22, 0.07],
-        ["action_08", 26, 0.11],
-        ["action_09", 48, 0.13],
-        ["action_10", 34, 0.27],
-        ["action_11", 42, 0.17],
-        ["action_12", 110, 0.09],
-        ["action_13", 38, 0.23],
-        ["action_14", 14, 0.01],
-        ["action_15", 18, 0.03],
-        ["action_16", 8, 0.08],
-        ["action_17", 4, 0.12],
-        ["action_18", 10, 0.14],
-        ["action_19", 24, 0.21],
-        ["action_20", 114, 0.18],
-    ]
+def glouton(donnees):
+    """
+    Algorithme triant les données de façon décroissante, et les ajoute jusqu'a la dépense maximum
+    """
 
     liste_selection = []
     achat_max = 500
     montant_max_itere = 0
     gain_max = 0
-    
-    for i in donnees: # ajoute le gain par action à la liste données
+
+    for i in donnees:  # ajoute le gain par action à la liste données
         gain = i[1] * i[2]
         i.append(gain)
-    
-    donnees_sorted = sorted(donnees, key=operator.itemgetter(2), reverse=True) # classe les actions par rentabilité
+
+    donnees_sorted = sorted(donnees, key=operator.itemgetter(2), reverse=True)  # classe les actions par rentabilité
 
     for i in donnees_sorted:
         if achat_max - i[1] > 0:
@@ -56,35 +32,41 @@ def glouton(donnees):
     print("Les actions sont :")
     print([i[0] for i in liste_selection])
     print()
-    
 
 
-def bruteforce_3(depense_max, donnees, lst_actions_selectionees = []):
-  
+def bruteforce(depense_max, donnees, lst_actions_selectionees=[]):
+    """
+    Algorithme de force brute, essayant toutes les combinaisons possible
+    """
+
     if donnees:
-        
-        # val1 et lst_val1 correspondent au résultat de bruteforce (rentabilité max, liste action), sans l'action courante
-        val1, lst_val1 = bruteforce_3(depense_max, donnees[1:], lst_actions_selectionees)
-        
-        action_selection = donnees[0]  # ici on selectionne une action dans la liste 
+
+        # val1 et lst_val1 correspondent au résultat de bruteforce (rentabilité max, liste action),
+        # sans l'action courante
+        val1, lst_val1 = bruteforce(depense_max, donnees[1:], lst_actions_selectionees)
+
+        action_selection = donnees[0]  # ici on selectionne une action dans la liste
 
         if action_selection[1] <= depense_max:
 
-            # On utilise bruteforce en enlevant le montant de l'action en cours à la dépense max, et on ajoute cette action dans la liste des actions selectionnées
-            val2, lst_val2 = bruteforce_3(depense_max - action_selection[1], donnees[1:], lst_actions_selectionees + [action_selection])
+            # On utilise bruteforce en enlevant le montant de l'action en cours à la dépense max,
+            # et on ajoute cette action dans la liste des actions selectionnées
+            val2, lst_val2 = bruteforce(depense_max - action_selection[1], donnees[1:],
+                                        lst_actions_selectionees + [action_selection])
 
             # On vérifie ici quelle est la meilleur rentabilité entre les deux solutions
             if val1 < val2:
                 return val2, lst_val2
         return val1, lst_val1
-    
+
     else:
-        # On renvoie à la fin la meilleur rentabilité total, ainsi que la liste des actions et le montant maximum trouvé
-        return f"la rentabilité maximum obtenue est : {round(sum([i[1] * i[2] for i in lst_actions_selectionees]), 2)}", \
+        # On renvoie à la fin la meilleur rentabilité total,
+        # ainsi que la liste des actions et le montant maximum trouvé
+        return f"la rentabilité maximum obtenue est : \
+            {round(sum([i[1] * i[2] for i in lst_actions_selectionees]), 2)}", \
             f"La depense maximum est : {sum([i[1] for i in lst_actions_selectionees])} euros, " \
-            f"avec ces actions: {[i[0] for i in lst_actions_selectionees]}"   
-    
-    
+            f"avec ces actions: {[i[0] for i in lst_actions_selectionees]}"
+
 
 lst_actions = [
         ["action_01", 20, 0.05],
@@ -109,7 +91,9 @@ lst_actions = [
         ["action_20", 114, 0.18],
     ]
 
-
+"""
+Les lignes suivantes permettent d'essayer l'algorithme de force brute sur un dataset de 1000 actions
+"""
 # with open('data/dataset1_Python+P7.csv') as dataset_1:
 #     dataset_1_reader = csv.reader(dataset_1)
 
@@ -124,9 +108,5 @@ lst_actions = [
 #     r[1] = float(r[1])
 #     r[2] = float(r[2])
 
-
-
-
-if __name__ == '__main__':
-    # glouton(lst_actions)
-    print(bruteforce_3(500, lst_actions))
+print(glouton(lst_actions))
+print(bruteforce(500, lst_actions))
--- optimized.py
+++ optimized.py
@@ -1,5 +1,5 @@
 import csv
-from memory_profiler import profile
+
 
 donnees = [
     ["action_01", 20, 0.05],
@@ -24,80 +24,16 @@ donnees = [
     ["action_20", 114, 0.18],
 ]
 
-for l in donnees:
-    l[2] = l[2] * 100
-
-
-def dynamic_20_actions(depense_max, donnees):
-    
-    """
-    On crée ici une matrice où les colonnes correspondent aux dépenses et les lignes aux actions.
-    Toutes les valeurs sont initialisés à 0
-    """
-    matrice = [[0 for x in range(depense_max + 1)] for x in range(len(donnees) + 1)]
-
-    # On fait ensuite une boucle sur les lignes actions
-    for l in range(1, len(donnees) + 1):
-        
-        # On fait ici une boucle sur les colonnes dépenses
-        for c in range(1, depense_max + 1):
-            # On vérifie que le coût de l'action est inférieur à la colonne dépense où l'on se situe
-            if donnees[l-1][1] <= c: 
-                
-                """
-                On rentre dans la matrice le maximum entre :
-                 - la rentabilité max de la ligne précédente -> matrice[l-1][c]
-                 - la rentabilité de la dépense courante + (la solution optimisée + la dépense de la ligne d'avant) -> donnees[l-1][2] + matrice[l-1][c-donnees[l-1][1]]
-                """
-
-                             #  max (rentablilité courante  +  rentabilité ligne précedente arrivant au poids max        ,   rentabilité max de la ligne précedente
-                matrice[l][c] = max(     donnees[l-1][2]    +           matrice[l-1][c-donnees[l-1][1]]                  ,          matrice[l-1][c])
-            
-            # Si la dépense est supérieure à la dépense max, on récupére la solution de la ligne précédente
-            else:
-                matrice[l][c] = matrice[l-1][c]
-
-
-    # Rechercher les actions en fonction du résultats
-    w = depense_max
-    n = len(donnees)
-
-    actions_selection = []
-    # Pour vérifier les actions correspondant au meilleurs résultat, on vérifie si l'action en cours est 
-
-    solution = float(matrice[-1][-1])
-    # Tant que la dépense est >= 0 et qu'il reste des actions
-    while w >= 0 and n >=0:
-
-        # ici on récupére la dernière action
-        a = donnees[n-1]
-
-        # Si la rentabilté en cours  == la rentabilité de la valeur en cours - la rentabilité de la valeur diminué de la dépense d'avant,
-        # alors on sait que cette action est à ajouté dans la liste
-        if matrice[n][w] == matrice[n-1][w -a[1]] + a[2]:
-            actions_selection.append(a)
-            
-            # on réduit ensuite la dépense max de la valeur de l'action selectionnée
-            w -= a[1]
-
-        # ensuite on passe à l'élément suivant
-        n -= 1
-
-    depense_total_selection = 0
-
-    for a in actions_selection:
-        depense_total_selection += a[1]
-
-
-    return (
-        f"--------------------------------------------------------------\n\n"
-        f"la rentabilité maximum obtenue est : {matrice[-1][-1]}\n\n"
-        f"La depense maximum est : {depense_total_selection}\n\n"
-        f"Avec ces actions: {[i[0] for i in actions_selection]}\n"   
-    )
+for line in donnees:
+    line[1] = line[1] * 100
+    line[2] = line[2] * 10000
 
 
 def csv_to_list_and_clean_datas(csv_file):
+    """
+    Fonction copiant les données d'un fichier csv vers une liste, et efface les données
+    erronées (inférieur ou égal à 0).
+    """
 
     with open(csv_file) as dataset_1:
         dataset_1_reader = csv.reader(dataset_1)
@@ -107,70 +43,70 @@ def csv_to_list_and_clean_datas(csv_file):
         for row in dataset_1_reader:
             dataset.append(row)
 
-    dataset.pop(0)    
+    dataset.pop(0)
 
     dataset_clean = []
 
     for row in dataset:
-    
+
         row[2] = int(round(float(row[1]) * (float(row[2]) / 100), 2) * 100)
         row[1] = int(float(row[1]) * 100)
-               
+
         if row[1] > 0 and row[2] > 0:
-                dataset_clean.append(row)
- 
+            dataset_clean.append(row)
+
     return dataset_clean
 
 
-def dynamic_dataset(depense_max, donnees):
-    
+def dynamic(depense_max, donnees):
     """
-    On crée ici une matrice où les colonnes correspondent aux dépenses et les lignes aux actions.
-    Toutes les valeurs sont initialisés à 0
+    Algorithme de programmation dynamique, sauvegardant le meilleur résultat à chaque itération,
+    et le comparant avec le meilleur résultat précédent.
     """
+
+    # On crée ici une matrice où les colonnes correspondent aux dépenses et les lignes aux actions.
+    # Toutes les valeurs sont initialisés à 0
     matrice = [[0 for x in range(depense_max + 1)] for x in range(len(donnees) + 1)]
 
     # On fait ensuite une boucle sur les lignes actions
-    for l in range(1, len(donnees) + 1):
-        
+    for line in range(1, len(donnees) + 1):
+
         # On fait ici une boucle sur les colonnes dépenses
         for c in range(1, depense_max + 1):
             # On vérifie que le coût de l'action est inférieur à la colonne dépense où l'on se situe
-            if donnees[l-1][1] <= c: 
-                
-                """
-                On rentre dans la matrice le maximum entre :
-                 - la rentabilité max de la ligne précédente -> matrice[l-1][c]
-                 - la rentabilité de la dépense courante + (la solution optimisée + la dépense de la ligne d'avant) -> donnees[l-1][2] + matrice[l-1][c-donnees[l-1][1]]
-                """
+            if donnees[line-1][1] <= c:
 
-                             #  max (rentablilité courante  +  rentabilité ligne précedente arrivant au poids max        ,   rentabilité max de la ligne précedente
-                matrice[l][c] = max(     donnees[l-1][2]    +           matrice[l-1][c-donnees[l-1][1]]                  ,          matrice[l-1][c])
-            
+                # On rentre dans la matrice le maximum entre :
+                #  - la rentabilité max de la ligne précédente
+                #       -> matrice[l-1][c]
+                #  - la rentabilité de la dépense courante + (la solution optimisée de la dépense de la ligne d'avant)
+                #       -> donnees[l-1][2] + matrice[l-1][c-donnees[l-1][1]]
+
+                #  max (rentablilité courante + rentabilité ligne précedente arrivant au poids max,
+                # rentabilité max de la ligne précedente
+                matrice[line][c] = max(donnees[line-1][2] + matrice[line-1][c-donnees[line-1][1]], matrice[line-1][c])
+
             # Si la dépense est supérieure à la dépense max, on récupére la solution de la ligne précédente
             else:
-                matrice[l][c] = matrice[l-1][c]
-
+                matrice[line][c] = matrice[line-1][c]
 
     # Rechercher les actions en fonction du résultats
     w = depense_max
     n = len(donnees)
-
     actions_selection = []
-    # Pour vérifier les actions correspondant au meilleurs résultat, on vérifie si l'action en cours est 
 
-    solution = float(matrice[-1][-1] / 100)
-    # Tant que la dépense est >= 0 et qu'il reste des actions
-    while w >= 0 and n >=0:
+    # Tant que la dépense est >= 0 et qu'il reste des actions à parcourir
+    while w >= 0 and n >= 0:
 
-        # ici on récupére la dernière action
+        # ici on récupére la dernière action, on parcours la liste à l'envers
         a = donnees[n-1]
 
-        # Si la rentabilté en cours  == la rentabilité de la valeur en cours - la rentabilité de la valeur diminué de la dépense d'avant,
+        # Si la rentabilté en cours  == la rentabilité de la valeur en cours -
+        #                               la rentabilité de la valeur diminué de la dépense d'avant,
         # alors on sait que cette action est à ajouté dans la liste
-        if matrice[n][w] == matrice[n-1][w -a[1]] + a[2]:
+        if matrice[n][w] == matrice[n-1][w - a[1]] + a[2]:
             actions_selection.append(a)
-            
+
             # on réduit ensuite la dépense max de la valeur de l'action selectionnée
             w -= a[1]
 
@@ -186,11 +122,15 @@ def dynamic_dataset(depense_max, donnees):
         f"--------------------------------------------------------------\n\n"
         f"la rentabilité maximum obtenue est : {matrice[-1][-1] / 100}\n\n"
         f"La depense maximum est : {depense_total_selection / 100}\n\n"
-        f"Avec ces actions: {[i[0] for i in actions_selection]}\n"   
+        f"Avec ces actions: {[i[0] for i in actions_selection]}\n"
     )
 
 
-def extract_sienna_datas(sienna_file, csv_file):
+def dynamic_with_sienna_datas(sienna_file, csv_file):
+    """
+    On applique ici les fonctions précédente sur les fichiers de Sienna,
+    et l'on renvoie les résultats
+    """
 
     sienna_s_list = []
 
@@ -203,19 +143,18 @@ def extract_sienna_datas(sienna_file, csv_file):
     dataset = csv_to_list_and_clean_datas(csv_file)
 
     dataset_to_compare = []
-    
+
     for i in dataset:
         for j in sienna_s_list:
             if i[0] == j:
                 dataset_to_compare.append(i)
-    
-    return dynamic_dataset(50000, dataset_to_compare)
+
+    return dynamic(50000, dataset_to_compare)
 
 
-# print(dynamic_20_actions(500, donnees))
-# print(dynamic_dataset(50000, csv_to_list_and_clean_datas('data/dataset2_Python+P7.csv')))
+print(dynamic(50000, donnees))
+print(dynamic(50000, csv_to_list_and_clean_datas('data/dataset1_Python+P7.csv')))
+print(dynamic(50000, csv_to_list_and_clean_datas('data/dataset2_Python+P7.csv')))
 
-# print(extract_sienna_datas('data/solution1_Python+P7.txt', 'data/dataset1_Python+P7.csv'))
-# print(extract_sienna_datas('data/solution2_Python+P7.txt', 'data/dataset2_Python+P7.csv'))
-
-
+print(dynamic_with_sienna_datas('data/solution1_Python+P7.txt', 'data/dataset1_Python+P7.csv'))
+print(dynamic_with_sienna_datas('data/solution2_Python+P7.txt', 'data/dataset2_Python+P7.csv'))
